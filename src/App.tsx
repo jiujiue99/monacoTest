7e68fce78947183cc9aa072025696d096bb98904
xuanjiu <369786460@qq.com>
2019-12-14 19:47:03 +0800

    scss loader and webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MonacoEditor from 'react-monaco-editor';
+import './testScss.scss'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const code = '// type your code...';
+    const options = {
+      selectOnLineNumbers: true
+    };
+    return (
+      <div>
+        <MonacoEditor
+          width="800"
+          height="600"
+          language="mysql"
+          theme="vs-dark"
+          value={code}
+          options={options}
+        />
+        <div className="scss-class">
+          scss style div
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
